7ec7829f2bd352d7b709721e2b90f0f506847089
BELDEN <beldenlew@gmail.com>
2026-04-04 02:34:27 +0800

    Update Potion.java

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -5,4 +5,12 @@ import engine.BattleEngine;
 
 public class Potion implements Item {
   @Override
-  
+  public void apply(Combatant user, Combatant target, BattleEngine engine) {
+    user.heal(100);
+  }
+
+  @Override 
+  public String getName() {
+    return "Potion";
+  }
+}
