c05cdd21720b4c718e60fcac8fdf710821fb7a50
kushmics <kushmics@gmail.com>
2026-04-03 19:11:44 +0800

    oops combatant getters, takeDamage method for combatant asw

--- src/combatants/Combatant.java
+++ src/combatants/Combatant.java
@@ -24,4 +24,25 @@ public abstract class Combatant {
         this.activeEffects = new ArrayList<>();
         this.invulnerable = false;
     }
+
+    public String getName() { return name; }
+    public int getHp() { return hp; }
+    public int getMaxHp() { return maxHp; }
+    public int getAttack() { return attack; }
+    public void setAttack(int attack) { this.attack = attack; }
+    public int getDefense() { return defense; }
+    public void setDefense(int defense) { this.defense = defense; }
+    public int getSpeed() { return speed; }
+    public boolean isInvulnerable() { return invulnerable; }
+    public void setInvulnerable(boolean invulnerable) { this.invulnerable = invulnerable; }
+
+    public void takeDamage(int damage) {
+        if (invulnerable) {
+            System.out.println(name + " takes 0 damage (Invulnerable).");
+            return;
+        }
+        this.hp -= damage;
+        if (this.hp < 0) this.hp = 0;
+        System.out.println(name + " takes " + damage + " damage! HP is now " + this.hp + "/" + this.maxHp + ".");
+    }
 }
