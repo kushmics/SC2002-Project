38e783d03ee4db03de903d87fee6cf349bdb276d
TehBingLessSugar <tanruiheng04@gmail.com>
2026-04-05 12:08:45 +0800

    Create ExecuteSpecialSkillAction.java
    player's class specific skill with a 3 turn cooldown

---
+++ src/actions/ExecuteSpecialSkillAction.java
@@ -0,0 +1,25 @@
+package actions;
+
+import combatants.Combatant;
+import combatants.Player;
+import engine.BattleEngine;
+
+public class ExecuteSpecialSkillAction implements Action {
+    @Override
+    public void execute(Combatant actor, Combatant target, BattleEngine engine) {
+        if (actor instanceof Player) {
+            Player p = (Player) actor;
+            if (p.canUseSpecialSkill()) {
+                p.getSpecialSkill().execute(actor, target, engine);
+                p.setCooldown(p.getMaxCooldown());
+            } else {
+                System.out.println("Special skill is on cooldown!");
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Execute Special Skill";
+    }
+}
