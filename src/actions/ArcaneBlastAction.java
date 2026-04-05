eb0fbff73d663573a5fc39677f4a72d72d0a8c3b
TehBingLessSugar <tanruiheng04@gmail.com>
2026-04-06 01:17:35 +0800

    deal BasicAttack damage to all enemies an +10 ATK per kill

--- src/actions/ArcaneBlastAction.java
+++ src/actions/ArcaneBlastAction.java
@@ -1,5 +1,34 @@
 package actions;
 
+import combatants.Combatant;
+import engine.BattleEngine;
+import effects.ArcaneBlastBuff;
+import java.util.List;
+
 public class ArcaneBlastAction implements Action {
+    @Override
+    public void execute(Combatant actor, Combatant target, BattleEngine engine) {
+        System.out.println(actor.getName() + " casts Arcane Blast!");
+        List<Combatant> enemies = engine.getAliveEnemies();
+        int defeatedCount = 0;
+        
+        for (Combatant enemy : enemies) {
+            int damage = Math.max(0, actor.getAttack() - enemy.getDefense());
+            enemy.takeDamage(damage);
+            System.out.println("-> Hits " + enemy.getName() + " for " + damage + " damage.");
+            if (!enemy.isAlive()) {
+                defeatedCount++;
+                System.out.println("-> " + enemy.getName() + " was defeated by Arcane Blast!");
+            }
+        }
+        
+        if (defeatedCount > 0) {
+            actor.addStatusEffect(new ArcaneBlastBuff(defeatedCount * 10));
+        }
+    }
     
+    @Override
+    public String getName() {
+        return "Arcane Blast";
+    }
 }
