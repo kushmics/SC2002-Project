74f7c7d856684f94031d834b20ce3cab50acd81e
TehBingLessSugar <tanruiheng04@gmail.com>
2026-04-05 15:42:28 +0800

    deal BasicAttack damage and apply Stun to target

--- src/actions/ShieldBashAction.java
+++ src/actions/ShieldBashAction.java
@@ -1,5 +1,20 @@
 package actions;
 
+import combatants.Combatant;
+import engine.BattleEngine;
+import effects.Stun;
+
 public class ShieldBashAction implements Action{
+    @Override
+    public void execute(Combatant actor, Combatant target, BattleEngine engine) {
+        int damage = Math.max(0, actor.getAttack() - target.getDefense());
+        System.out.println(actor.getName() + " uses Shield Bash on " + target.getName() + "!");
+        target.takeDamage(damage);
+        target.addStatusEffect(new Stun(2));
+    }
     
+    @Override
+    public String getName() {
+        return "Shield Bash";
+    } 
 }
