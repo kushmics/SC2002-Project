c27caf39d551358db2371c44b55e4da5fd34cd29
tarunr005 <tarunram0912@gmail.com>
2026-04-08 15:38:10 +0800

    Update GameCLI.java

--- src/cli/GameCLI.java
+++ src/cli/GameCLI.java
@@ -1,1 +1,32 @@
+package cli;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+import combatants.*;
+import engine.*;
+import actions.*;
+import items.*;
+
+public class GameCLI {
+    private Scanner scanner;
+
+    public GameCLI() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        boolean running = true;
+        while (running) {
+            System.out.println("=== Turn-Based Combat Arena ===");
+            System.out.println("1. Start Game");
+            System.out.println("2. Exit");
+            int choice = getIntInput(1, 2);
+            if (choice == 1) {
+                playGame();
+            } else {
+                running = false;
+                System.out.println("Goodbye!");
+            }
+        }
+    }
