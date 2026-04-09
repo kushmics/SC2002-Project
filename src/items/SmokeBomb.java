f63aae9368f40a428cb4ac8d3998241f33f29442
BELDEN <beldenlew@gmail.com>
2026-04-09 18:19:03 +0800

    Create SmokeBomb.java

---
+++ src/items/SmokeBomb.java
@@ -0,0 +1,17 @@
+package items;
+
+import combatants.Combatants;
+import engine.BattleEngine;
+import effects.SmokeBombEffect;
+
+public class SmokeBomb implements Item {
+  @Override
+  public void apply(Combatant user, Combatant target, BattleEngine engine) {
+    user.addStatusEffect(new SmokeBombEffect(2));
+  }
+
+  @Override
+  public String getName() {
+    return "Smoke Bomb";
+  }
+}
