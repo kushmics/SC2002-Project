5f626e0c4c3c60110d30b62cb95c7b357cfce6b9
TehBingLessSugar <tanruiheng04@gmail.com>
2026-04-10 20:20:06 +0800

    +10 DEF for current round and next round

---
+++ src/actions/DefendAction.java
@@ -0,0 +1,18 @@
+package actions;
+
+import combatants.Combatant;
+import engine.BattleEngine;
+import effects.DefendBuff;
+
+public class DefendAction implements Action {
+    @Override
+    public void execute(Combatant actor, Combatant target, BattleEngine engine) {
+        System.out.println(actor.getName() + " defends! Defense increased.");
+        actor.addStatusEffect(new DefendBuff(2));
+    }
+    
+    @Override
+    public String getName() {
+        return "Defend";
+    }
+}
