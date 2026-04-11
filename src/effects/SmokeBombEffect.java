ae72f9ec3fcd66b118c0d38771170265c53d97d9
nsd06 <ninadonau1@gmail.com>
2026-04-11 20:43:37 +0800

    Implement StatusEffect methods

--- src/effects/SmokeBombEffect.java
+++ src/effects/SmokeBombEffect.java
@@ -8,4 +8,27 @@ public class SmokeBombEffect implements StatusEffect {
     public SmokeBombEffect(int duration) {
         this.duration = duration;
     }
+
+    @Override
+    public void onApply(Combatant target) {}
+
+    @Override
+    public void tick(Combatant target) {}
+
+    public void onRemove(Combatant target) {}
+
+    @Override
+    public boolean isExpired() {
+        return duration <= 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Smoke Bomb Effect";
+    }
+
+    @Override
+    public boolean preventsAction() {
+        return false;
+    }
 }
