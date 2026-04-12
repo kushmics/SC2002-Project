c5249630ca6f0c5b224e4ea284851c96ed104fe2
nsd06 <ninadonau1@gmail.com>
2026-04-12 13:26:00 +0800

    Create ArcaneBlastBuff skeleton

--- src/effects/ArcaneBlastBuff.java
+++ src/effects/ArcaneBlastBuff.java
@@ -2,5 +2,29 @@ package effects;
 import combatants.Combatant;
 
 public class ArcaneBlastBuff implements StatusEffect {
+
+    @Override
+    public void onApply(Combatant target) {}
+
+    @Override
+    public void tick(Combatant target) {}
+
+    @Override
+    public void onRemove(Combatant target) {}
+
+    @Override
+    public boolean isExpired() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return "Arcane Blast Buff";
+    }
+
+    @Override
+    public boolean preventsAction() {
+        return false;
+    }
     
 }
