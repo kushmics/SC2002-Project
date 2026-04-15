eee918ad1cbd34f06caa8ade9144c20cbaa2f7b4
kushmics <kushmics@gmail.com>
2026-04-16 03:33:17 +0800

    poison cloud effect constructor

--- src/effects/PoisonCloudEffect.java
+++ src/effects/PoisonCloudEffect.java
@@ -1,5 +1,13 @@
 package effects;
 
-public class PoisonCloudEffect {
+import combatants.Combatant;
+
+public class PoisonCloudEffect implements StatusEffect {
+    private int duration;
+    private static final int POISON_DAMAGE = 15;
+
+    public PoisonCloudEffect(int duration) {
+        this.duration = duration;
+    }
     
 }
