1cfe8d0c834f619b3349ebf71312acfd1d5acb0a
kushmics <kushmics@gmail.com>
2026-04-16 03:31:14 +0800

    poison cloud item finished

--- src/items/PoisonCloud.java
+++ src/items/PoisonCloud.java
@@ -1,5 +1,22 @@
 package items;
 
-public class PoisonCloud {
+import combatants.Combatant;
+import engine.BattleEngine;
+import effects.PoisonCloudEffect;
+
+public class PoisonCloud implements Item {
+    @Override
+    public void apply(Combatant user, Combatant target, BattleEngine engine) {
+        target.addStatusEffect(new PoisonCloudEffect(3)); // Poison for 3 turns
+    }
     
+    @Override
+    public String getName() {
+        return "Poison Cloud";
+    }
+
+    @Override
+    public boolean requiresTarget() {
+        return true;
+    }
 }
