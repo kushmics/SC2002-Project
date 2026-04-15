f2f83aeba5cdb058cb0f5b684ea1b4186074cf68
kushmics <kushmics@gmail.com>
2026-04-16 03:35:21 +0800

    poison cloud effect implements the rest of statuseffect methods

--- src/effects/PoisonCloudEffect.java
+++ src/effects/PoisonCloudEffect.java
@@ -10,4 +10,37 @@ public class PoisonCloudEffect implements StatusEffect {
         this.duration = duration;
     }
     
+    @Override
+    public void onApply(Combatant target) {
+        System.out.println(target.getName() + " is engulfed in a Poison Cloud! (Takes " + POISON_DAMAGE + " damage per turn for " + duration + " turns)");
+    }
+    
+    @Override
+    public void tick(Combatant target) {
+        if (duration > 0) {
+            System.out.println(target.getName() + " takes " + POISON_DAMAGE + " poison damage from the Poison Cloud!");
+            target.takeDamage(POISON_DAMAGE);
+        }
+        duration--;
+    }
+    
+    @Override
+    public void onRemove(Combatant target) {
+        System.out.println(target.getName() + "'s Poison Cloud effect dissipates.");
+    }
+    
+    @Override
+    public boolean isExpired() {
+        return duration <= 0;
+    }
+    
+    @Override
+    public String getName() {
+        return "Poison Cloud";
+    }
+    
+    @Override
+    public boolean preventsAction() {
+        return false;
+    }
 }
